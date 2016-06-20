5b19b12f6163582f9d6d73fd2fac0739d9926f96
André Arruda <andrearruda82@gmail.com>
2016-06-20 09:57:02 -0300

    ajuste no formulario

--- app/src/Action/IndicadorAction.php
+++ app/src/Action/IndicadorAction.php
@@ -35,66 +35,23 @@ class IndicadorAction
 
         if($request->isPost())
         {
-            $data_form = array(
-                'fieldset_informacoes' => array(
-                    'date' => $request->getParam('date'),
-                    'responsible' => $request->getParam('responsible')
-                ),
-                'fieldset_periodo' => array(
-                    'period_first_initial' => '01/' . $request->getParam('period_first_initial'),
-                    'period_first_end' => '01/' . $request->getParam('period_first_end'),
-                    'period_second_initial' => '01/' . $request->getParam('period_second_initial'),
-                    'period_second_end' => '01/' . $request->getParam('period_second_end'),
-                ),
-                'fieldset_comparativo_grupobenner' => array(
-                    'groupbenner_revenues_target' => $request->getParam('groupbenner_revenues_target'),
-                    'groupbenner_revenues_initial' => $request->getParam('groupbenner_revenues_initial'),
-                    'groupbenner_revenues_end' => $request->getParam('groupbenner_revenues_end'),
-                    'groupbenner_revenues_percentage' => $request->getParam('groupbenner_revenues_percentage'),
+            $data_form = $request->getParsedBody();
 
-                    'groupbenner_ebtida_target' => $request->getParam('groupbenner_ebtida_target'),
-                    'groupbenner_ebtida_initial' => $request->getParam('groupbenner_ebtida_initial'),
-                    'groupbenner_ebtida_end' => $request->getParam('groupbenner_ebtida_end'),
-                    'groupbenner_ebtida_percentage' => $request->getParam('groupbenner_ebtida_percentage'),
-
-                    'groupbenner_netprofit_target' => $request->getParam('groupbenner_netprofit_target'),
-                    'groupbenner_netprofit_initial' => $request->getParam('groupbenner_netprofit_initial'),
-                    'groupbenner_netprofit_end' => $request->getParam('groupbenner_netprofit_end'),
-                    'groupbenner_netprofit_percentage' => $request->getParam('groupbenner_netprofit_percentage'),
-                ),
-                'fieldset_comparativo_operadorasdesaude' => array(
-                    'healthoperators_revenues_target' => $request->getParam('healthoperators_revenues_target'),
-                    'healthoperators_revenues_initial' => $request->getParam('healthoperators_revenues_initial'),
-                    'healthoperators_revenues_end' => $request->getParam('healthoperators_revenues_end'),
-                    'healthoperators_revenues_percentage' => $request->getParam('healthoperators_revenues_percentage'),
-
-                    'healthoperators_ebtida_target' => $request->getParam('healthoperators_ebtida_target'),
-                    'healthoperators_ebtida_initial' => $request->getParam('healthoperators_ebtida_initial'),
-                    'healthoperators_ebtida_end' => $request->getParam('healthoperators_ebtida_end'),
-                    'healthoperators_ebtida_percentage' => $request->getParam('healthoperators_ebtida_percentage'),
-
-                    'healthoperators_netprofit_target' => $request->getParam('healthoperators_netprofit_target'),
-                    'healthoperators_netprofit_initial' => $request->getParam('healthoperators_netprofit_initial'),
-                    'healthoperators_netprofit_end' => $request->getParam('healthoperators_netprofit_end'),
-                    'healthoperators_netprofit_percentage' => $request->getParam('healthoperators_netprofit_percentage'),
-                )
-            );
+            foreach($data_form['fieldset_periodo'] as $key => $item)
+            {
+                $data_form['fieldset_periodo'][$key] = '01/' . $item;
+            }
 
             $form->setData($data_form);
             if($form->isValid())
             {
-
+                echo 'VALID' . PHP_EOL;
+                var_dump($data_form);
+                die;
             }
             else
             {
-                $data_form['fieldset_periodo'] = array(
-                    'period_first_initial' => $request->getParam('period_first_initial'),
-                    'period_first_end' => $request->getParam('period_first_end'),
-                    'period_second_initial' => $request->getParam('period_second_initial'),
-                    'period_second_end' => $request->getParam('period_second_end'),
-                );
-
-                $form->setData($data_form);
+                $form->setData($request->getParsedBody());
             }
         }
 
